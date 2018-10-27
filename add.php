7b00f30609f580bb5a8c72b323cb250106247649
ABDUL KADHAR <43913066+abdulkadhar0435@users.noreply.github.com>
2018-10-27 16:46:09 +0300

    Php Adding

---
+++ add.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+</head>
+<body>
+	<form method="post" action="">
+		<input type="number" name="firstval">
+		<input type="number" name="secondval">
+		<input type="submit" name="SubmitButton">
+		Result : <?php if(isset($message)){ echo $message; } ?>
+	</form>
+</body>
+</html>
+
+<?php
+if(isset($_POST['SubmitButton'])){ 
+  $firstval = $_POST['firstval']; 
+  $secondval = $_POST['secondval']; 
+ echo $total = $firstval + $secondval;
+  $message = "Success! Total: ".$total;
+}  
+
+?>
